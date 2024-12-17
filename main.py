0f7dfded4231b91b4dbb591fd06186f6b3b117c1
Canuski <ojalexander24@gmail.com>
2024-12-17 20:51:30 +0100

    added vuln python and requirment

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("Hello world-test!")
-thats weird
+secret_password = "SuperSecret123"
+print("Hard-coded secret:", secret_password)
+
+user_input = "2 + 2"
+print("Result:", eval(user_input))  # Kwetsbare code
