8fc1d4a1895e3698778fcf352705c064f76dae3c
Canuski <ojalexander24@gmail.com>
2024-12-17 20:54:33 +0100

    test for secrets detection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 secret_password = "SuperSecret123"
 print("Hard-coded secret:", secret_password)
 
-user_input = "2 + 2"
-print("Result:", eval(user_input))  # Kwetsbare code
+# bandit test 
+# user_input = "2 + 2"
+# print("Result:", eval(user_input))
